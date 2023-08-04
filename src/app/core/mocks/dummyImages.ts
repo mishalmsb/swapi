f14857885942c3fcef205ccaaf17be3475c9508f
Mishal Shashikant <mishalmsb@gmail.com>
2023-08-04 16:17:22 +0100

    replace dummy image

--- src/app/core/mocks/dummyImages.ts
+++ src/app/core/mocks/dummyImages.ts
@@ -1,5 +1,2 @@
 export const dummyImage: string =
-  'https://images.unsplash.com/photo-1624467906831-1f80d34ed5ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMzAxMzJ8MHwxfHNlYXJjaHwxfHxMdWtlJTIwU2t5d2Fsa2VyfGVufDB8MXx8fDE2OTA5OTgwMzV8MA&ixlib=rb-4.0.3&q=80&w=400';
-
-export const dummyImageTwo: string =
-  'https://images.unsplash.com/photo-1605590990745-21f711f58853?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMzAxMzJ8MHwxfHNlYXJjaHwxfHxBJTIwTmV3JTIwSG9wZXxlbnwwfDB8fHwxNjkxMDgxODMyfDA&ixlib=rb-4.0.3&q=80&w=400';
+  'https://images.unsplash.com/photo-1470083010285-12169927e64c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMzAxMzJ8MHwxfHNlYXJjaHwxfHxUaGUlMjBQaGFudG9tJTIwTWVuYWNlfGVufDB8MHx8fDE2OTEwODE4MzJ8MA&ixlib=rb-4.0.3&q=80&w=400';
